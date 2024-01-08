50d114eb6a5034094bbf31f0fc36f211f9644113
Daniela Soares <danielasmelo232@gmail.com>
2024-01-08 20:52:06 -0300

    projeto crud

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ purge: { 
+  content: [ 
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+   
+  ],
+  safelist: [
+    /^bg-/,
+    /^to-/,
+    /^from-/,
+  ]
+},
+  //darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extends:  {},
+  },
+  plugins: [],
+}
+
